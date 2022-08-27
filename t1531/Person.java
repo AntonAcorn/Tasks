e325f241f391465b660c18b120e0f8d232ce3261
AntonAcorn <ShustikovAnimation@gmail.com>
2022-08-27 22:05:38 +0300

    Class Person для задачи 1531
    Class Person для задачи 1531

---
+++ t1531/Person.java
@@ -0,0 +1,28 @@
+package t1531;
+
+public interface Person {
+    class User implements Person {
+        void live() {
+            System.out.println("I usually just live.");
+        }
+    }
+
+    class Loser implements Person {
+        void doNothing() {
+            System.out.println("I usually do nothing.");
+        }
+    }
+
+    class Coder implements Person {
+        void writeCode() {
+            System.out.println("I usually write code.");
+        }
+    }
+
+    class Proger implements Person {
+        void enjoy() {
+            System.out.println("It's a wonderful life!");
+        }
+    }
+}
+
---
+++ t1531/Solution.java
@@ -0,0 +1,29 @@
+package t1531;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/*
+User, Loser, Coder and Proger
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        Person person = null;
+        String key = null;
+
+        //тут цикл по чтению ключей, пункт 1
+        {
+            //создаем объект, пункт 2
+
+            doWork(person); //вызываем doWork
+
+        }
+    }
+
+    public static void doWork(Person person) {
+        // пункт 3
+    }
+}
+
