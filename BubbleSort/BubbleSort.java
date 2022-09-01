67d476db72a72c938d3615fea789e78338832e06
AntonAcorn <ShustikovAnimation@gmail.com>
2022-09-02 00:46:32 +0300

    Create BubbleSort.java
    BubbleSort

---
+++ BubbleSort/BubbleSort.java
@@ -0,0 +1,20 @@
+package BubbleSort;
+public class BubbleSort {
+    public static void main(String[] args) {
+        int [] array = new int []{4,3,7,0,5,2,3,5,7,8,};
+        int temp;
+        for (int i = 0; i < array.length; i++){
+            for(int j = 0; j < array.length-1; j++){
+                if (array [j] > array[ j+1]){
+                    temp = array[j];
+                    array [j] = array [j +1];
+                    array [j+1] = temp;
+                }
+            }
+        }
+        for (int i = 0; i < array.length; i++){
+            System.out.println(array[i]);
+        }
+
+    }
+}
