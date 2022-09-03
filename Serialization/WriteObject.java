3a6953e56112d3b410991ed2c1fd7e8585ee8122
AntonAcorn <ShustikovAnimation@gmail.com>
2022-09-03 20:49:53 +0300

    Update WriteObject.java

--- Serialization/WriteObject.java
+++ Serialization/WriteObject.java
@@ -1,11 +1,10 @@
 package Serialization;
 
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 
-public class WtriteObject {
+public class WriteObject {
     public static void main(String[] args) {
         Person person1 = new Person(1, "Bob");
         Person person2 = new Person(2,"Mike");
