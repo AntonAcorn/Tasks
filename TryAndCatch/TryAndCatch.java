67244a8afa4f508288ed95f767b99e5f29993773
AntonAcorn <ShustikovAnimation@gmail.com>
2022-09-03 16:11:07 +0300

    Try and Catch if file is not found

---
+++ TryAndCatch/TryAndCatch.java
@@ -0,0 +1,22 @@
+package TryAndCatch;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class TryAndCatch {
+    public static void main(String[] args){
+        try{readFile();
+        }catch (Exception e){
+            System.out.println("File is not found");
+        }
+
+
+
+    }
+    public static void readFile() throws IOException {
+        File file = new File("sdfsdf");
+        Scanner scanner = new Scanner(file);
+        String lines = scanner.nextLine();
+    }
+}
