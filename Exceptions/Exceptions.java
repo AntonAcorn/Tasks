8b113a6d9d1c7155d00c7f98ee09d12e7c30affb
AntonAcorn <ShustikovAnimation@gmail.com>
2022-09-03 16:28:56 +0300

    Creating of a new Exception

---
+++ Exceptions/Exceptions.java
@@ -0,0 +1,16 @@
+package Exceptions;
+
+import java.util.Scanner;
+
+public class Exceptions {
+    public static void main (String [] args) throws ScannerException{
+        Scanner scanner = new Scanner(System.in);
+
+        while(true){
+            int number = Integer.parseInt(scanner.nextLine());
+            if (number != 0){
+                throw new ScannerException("User entered something except 0");
+            }
+        }
+    }
+}
---
+++ Exceptions/ScannerException.java
@@ -0,0 +1,7 @@
+package Exceptions;
+
+public class ScannerException extends Exception {
+    public ScannerException(String message){
+
+    }
+}
