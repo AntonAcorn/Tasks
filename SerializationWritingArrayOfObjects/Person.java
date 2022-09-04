d54912fa5e8ad0e71076505bf1884e8d8cc89418
AntonAcorn <ShustikovAnimation@gmail.com>
2022-09-04 10:55:32 +0300

    Writing and Reading Array of objects with for

---
+++ SerializationWritingArrayOfObjects/Person.java
@@ -0,0 +1,25 @@
+package SerializationWritingArrayOfObjects;
+
+import java.io.Serializable;
+
+public class Person implements Serializable {       //НЕ ЗАБЫТЬ, ИНАЧЕ НЕ БУДЕТ РАБОТАТЬ
+    private int id;
+    private String name;
+
+    public Person(int id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String toString(){
+        return id + " : " + name;
+    }
+}
