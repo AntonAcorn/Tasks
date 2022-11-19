a132d7594c6c759148d4a0aca3b81f032d36d46c
AntonAcorn <ShustikovAnimation@gmail.com>
2022-11-19 19:27:34 +0300

    //Complete the solution so that the function will break up camel casing, using a space between words. //Example //"camelCasing"  =>  "camel Casing" //"identifier"   =>  "identifier" //""             =>  ""

---
+++ breakCamelCase/BreakCamelCase.java
@@ -0,0 +1,28 @@
+package breakCamelCase;
+
+import java.util.stream.Stream;
+
+//Complete the solution so that the function will break up camel casing, using a space between words.
+//Example
+//"camelCasing"  =>  "camel Casing"
+//"identifier"   =>  "identifier"
+//""             =>  ""
+public class BreakCamelCase {
+    public static void main(String[] args) {
+        System.out.println(camelCase("camelCasing"));
+
+
+    }
+    public static String camelCase(String input) {
+        String result = "";
+        char [] character = input.toCharArray();
+        for (int i = 0; i < character.length-1; i++) {
+            result = result + character[i];
+            if(Character.isUpperCase(character[i+1])){
+                result = result + " ";
+            }
+        }
+        result = result + character[input.length()-1];
+        return result;
+    }
+}
